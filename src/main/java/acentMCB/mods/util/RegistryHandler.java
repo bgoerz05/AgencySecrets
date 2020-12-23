09bd4b6bfc1220477e1f8d24e721956d8e79dcc5
bgoerz05 <54515340+bgoerz05@users.noreply.github.com>
2020-12-22 20:49:54 -0600

    made mod function and added Lunite Shard item

---
+++ src/main/java/acentMCB/mods/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package acentMCB.mods.util;
+
+import acentMCB.mods.AgencySecrets;
+import acentMCB.mods.item.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.ForgeRegistry;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AgencySecrets.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static final RegistryObject<Item> LUNITESHARD = ITEMS.register("lunite_shard", ItemBase::new);
+
+}
