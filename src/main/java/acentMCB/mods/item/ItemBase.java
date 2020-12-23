8ea97db7fa5848b553688d35e98125bf73ba0875
bgoerz05 <54515340+bgoerz05@users.noreply.github.com>
2020-12-23 14:15:01 -0600

    Added Creative Tab And Moved Shard To It.

--- src/main/java/acentMCB/mods/item/ItemBase.java
+++ src/main/java/acentMCB/mods/item/ItemBase.java
@@ -1,10 +1,11 @@
 package acentMCB.mods.item;
 
+import acentMCB.mods.AgencySecrets;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ItemBase extends Item {
     public ItemBase() {
-        super(new Item.Properties().group(ItemGroup.MATERIALS));
+        super(new Item.Properties().group(AgencySecrets.TAB));
     }
 }
